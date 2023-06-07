5931764121ec1d99aaef2df3bcf231b866497718
Vincent Fontaine <vincentfontaine.jobs@gmail.com>
2023-06-07 13:55:36 +0200

    reactivate console output

--- Gateau.Prod/Logger.cs
+++ Gateau.Prod/Logger.cs
@@ -4,16 +4,10 @@ namespace GateauKata;
 
 public class Logger : ILogger
 {
-    // public Logger()
-    // {
-    //     for (int i = 0; i < 20; i++)
-    //     {
-    //         Debug.WriteLine("");
-    //     }
-    // }
 
     public void log(string message)
     {
+        Console.Out.WriteLine(message);
         Debug.WriteLine(message);
     }
 }
